0fcccfd7a31c960d3cf553d9a6e0d5dc9bb776a4
Sahil Gaurkar <sahilgaurkar@gmail.com>
2023-02-24 11:18:43 +0000

    PD Controller

--- Client/Control/PDController.m
+++ Client/Control/PDController.m
@@ -1,3 +1,7 @@
+function [outputArg1,outputArg2] = PDController1(inputArg1,inputArg2)
+%PDCONTROLLER1 Summary of this function goes here
+%   Detailed explanation goes here
+
 clc; clear; close all;
 %Define Transfer Function
 
@@ -7,3 +11,9 @@ Ki = 0;
 
 C = pid(Kp, Ki, Kd);
 tf(C)
+
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
