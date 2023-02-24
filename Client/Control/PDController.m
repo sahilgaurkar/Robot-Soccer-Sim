5954beb0a6c5d59b938fc3a62e7e5e1bf5a3ae8e
Sahil Gaurkar <sahilgaurkar@gmail.com>
2023-02-24 15:32:50 +0000

    Template Added

--- Client/Control/PDController.m
+++ Client/Control/PDController.m
@@ -1,19 +1,33 @@
-function [outputArg1,outputArg2] = PDController1(inputArg1,inputArg2)
-%PDCONTROLLER1 Summary of this function goes here
-%   Detailed explanation goes here
+
+
 
 clc; clear; close all;
-%Define Transfer Function
 
-Kp = 1;
-Kd = 1;
+% SI Units are used
+maxVelocity = 310; % mm/s
+maxHeadingRate = 45; % deg/s
+
+%Define Transfer Function
+% Example System
+
+m = 1;
+b = 10;
+k = 20;
+F = 1;
+
+s = tf('s');
+P = 1/(s^2 + 10*s + 20);
+step(P)
+
+
+
+Kp = 300;
+Kd = 10;
 Ki = 0;
 
-C = pid(Kp, Ki, Kd);
-tf(C)
+C = pid(Kp, 0, Kd);
+T = feedback(C*P, 1)
 
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+t = 0:0.01:2;
+step(T,t)
 
