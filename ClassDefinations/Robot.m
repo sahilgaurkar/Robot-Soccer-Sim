799c483262607b68a90d89760ab5e9d8adbbc49e
Sahil Gaurkar <sahilgaurkar@gmail.com>
2023-02-25 14:05:32 +0000

    Updated Robot Class

--- ClassDefinations/Robot.m
+++ ClassDefinations/Robot.m
@@ -3,20 +3,77 @@ classdef Robot
     %   Detailed explanation goes here
     
     properties
-        Property1
+        %% All Units are in SI units
+        %% Identification
+        teamName = 'None'; % 'Home' | 'Away'
+        robotName = 'None'; % 'robot_1' to 'robot_n'
+        robotRole = 'None'; % 'Attacker' | 'Defender' | 'GoalKeeper'
+        %% Properties
+        bodyRadius = 0; % (mm)
+        weight = 0; % (kg)
+        kickableAreaRadius = 0; % (mm)
+        maxMoveAccleration = 0; % (mm/s^2)
+        maxTurnRate = 0; % (deg/s)
+        maxKickPower = 0; % Force (N)
+        %% Robot Movement
+        currentPose = [0, 0, 0]; % [X, Y, Angle]
+        destinationPose = [0, 0, 0]; % [X, Y, Angle]
+        accleration = 0; % (mm/s^2) (Negative | Positive values)
+        headingRate = 0; % (deg/s) (Negative | Positive values)
+        %% Command
+        kick = false; % true | false
+        kickPower = 0; % [0 - 100%] of maxKickPower
+        %% Feedback
+        isBallKickable = flase; % true | false
     end
     
     methods
-        function obj = Robot(inputArg1,inputArg2)
-            %UNTITLED Construct an instance of this class
+        function obj = Robot(teamName, robotName, robotRole)
+            % UNTITLED Construct an instance of this class
             %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+            
+            obj.teamName = teamName;
+            obj.robotName = robotName;
+            obj.robotRole = robotRole;
+        end
+
+        function obj = setIdentification(obj, teamName, robotName, robotRole)
+            % setIdentification is used to set Identifications Properties
+            %   Detailed explanation goes here
+            
+            obj.teamName = teamName;
+            obj.robotName = robotName;
+            obj.robotRole = robotRole;
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
+        function obj = setProperties(obj,bodyRadius, weight, kickableAreaRadius, maxMoveAccleration, maxTurnRate, maxKickPower)
+            % setProperties is used to set Robot Static Properties;
             %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+            
+            obj.bodyRadius = bodyRadius;
+            obj.weight = weight;
+            obj.kickableAreaRadius = kickableAreaRadius;
+            obj.maxMoveAccleration = maxMoveAccleration;
+            obj.maxTurnRate = maxTurnRate;
+            obj.maxKickPower = maxKickPower;
+
+        end
+
+        function obj = moveTo(obj,destinationPose)
+            % moveTo is used to set destinationPose;
+            %   Detailed explanation goes here
+        
+            obj.destinationPose = destinationPose;
+
+        end
+
+        function obj = kickBall(obj, kickPower)
+            % moveTo is used to set destinationPose;
+            %   Detailed explanation goes here
+        
+            obj.kick = true;
+            obj.kickPower = kickPower;
+            
         end
     end
 end
