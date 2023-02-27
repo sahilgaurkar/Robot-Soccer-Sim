0178e90f0532a9641fb6d7f4a495d0b586b484c5
Sahil Gaurkar <sahilgaurkar@gmail.com>
2023-02-27 00:55:55 +0000

    Accleration and Heading Controller

--- Client/Control/PDController.m
+++ Client/Control/PDController.m
@@ -1,78 +1,123 @@
 
-clc; clear; close all;
+clc; close all; clear;
 
 bot = Robot('Home', 'Bot_1', 'Attacker');
 bot.currentPose = [0, 0, 0];
-bot.destinationPose = [5, 5, 45];
-bot.accleration = 1;
-bot.headingRate = 45;
+bot.destinationPose = [1000, 1000, 90];
+bot.accleration = [0 0];
+bot.headingRate = 0;
+bot.currentVelocity = [0 0];
 
-bot.maxMoveAccleration = 50;
-bot.maxMoveVelocity = 5;
+bot.maxMoveAccleration = 300;
+bot.maxMoveVelocity = 300;
 bot.maxTurnRate = 45;
 
 
-% Define the PID controller parameters
-Kp = 0.0008; % Proportional gain
-Ki = 0; % Integral gain
-Kd = 0.302376; % Derivative gain
-N = 1.893;
+%% Accleration Controller define the PID controller parameters
+Kp = 0.0011;  %0.010; % Proportional gain
+Ki = 1.3291e-06; % Integral gain
+Kd = 0.1998;  %0.3458; % Derivative gain
+Tf = 0.4828 ; %0.3400;
+Ts = 0.1; % 100 ms Set the sample time for the controller
 
 % Initialize the PID controller
-pid_controller = pid(Kp, Ki, Kd);
+CAccleration = pid(Kp, Ki, Kd, Tf, Ts, 'IFormula','Trapezoidal');
 
-% Set the sample time for the controller
-T = 0.01; % 10 ms
-pid_controller.Ts = T;
+
+%% Heading Controller Define the PID controller parameters
+Kp = 1.7036; % Proportional gain
+Ki = 0; % Integral gain
+Kd = 0; % Derivative gain
+Tf = 0;
+Ts = 0.1; % 100 ms Set the sample time for the controller
+
+% Initialize the PID controller
+CHeading = pid(Kp, Ki, Kd, Tf, Ts, 'IFormula','Trapezoidal');
+
+
 
 % Initialize the robot's motion variables
 bot.currentVelocity = 0; % current velocity of the robot
 current_angular_velocity = 0; % current angular velocity of the robot (in degrees per second)
 
 % Define the simulation time and time step
-sim_time = 10; % 10 seconds
-time_step = 1; % 10 ms
+sim_time = 100; % 10 seconds
+time_step = 0.1; % 10 ms
 
 
 % Loop through the simulation time
 for t = 1:time_step:sim_time
 
-    % Calculate the error between the current position/heading and the desired position/heading
-    errorDistance = norm(bot.destinationPose(:,1:2) - bot.currentPose(:,1:2));
-    errorHeading = bot.destinationPose(3) - bot.currentPose(3);
+    %% Get Error
+    errorHeading = atan2d(bot.destinationPose(2) - bot.currentPose(2), bot.destinationPose(1) - bot.currentPose(1)) - bot.currentPose(3);
+    errorHeading = wrapTo360(errorHeading);
+    errorPosition = bot.destinationPose(1:2) - bot.currentPose(1:2);
+    errorDistance = pdist2(bot.destinationPose(1:2), bot.currentPose(1:2), 'euclidean');
 
+    %% Calculate the Heading Rate and Accleration using the PD
+    headingRateOutput = step(errorHeading*CHeading, time_step);
+    
+   
+        acclerationOutputX = step(errorDistance * CAccleration, time_step);
+        acclerationOutputY = step(errorDistance * CAccleration, time_step);
 
+    %% Set Heading Rate and Accleration of Bot
+    bot.headingRate = headingRateOutput(1);
+ 
+    if bot.currentVelocity < bot.maxMoveVelocity
+    bot.accleration = [acclerationOutputX(1), acclerationOutputY(1)];
 
+     else
+        bot.accleration = [0, 0];
+    end
 
-    % Calculate the desired acceleration and heading rate using the PID controller
-    %acceleration = pid_controller(errorDistance);
     
 
 
     %% Update Position based on Accleration and Heading Angle
     bot = doubleIntegrate(bot, time_step);
-%     plot(bot.currentPose(1), bot.currentPose(2), 'xb')
-%     hold on
-    
+
+
+
+
 
 end
 
 
 function obj = doubleIntegrate(obj, deltaT)
 
+
+%% integrate heading rate to get current Heading
 if obj.headingRate < obj.maxTurnRate
     obj.currentPose(3) = obj.currentPose(3) + obj.headingRate * deltaT;
 else
     obj.currentPose(3) = obj.currentPose(3) + obj.maxTurnRate * deltaT;
 end
 
-if obj.currentVelocity < obj.maxMoveVelocity
-    obj.currentVelocity = obj.currentVelocity + obj.accleration * deltaT;
-else
-    obj.currentVelocity = obj.maxMoveVelocity;
-end
+obj.currentVelocity = obj.currentVelocity + obj.accleration * deltaT;
 
-obj.currentPose(1) = obj.currentPose(1) + obj.currentVelocity* cosd(obj.currentPose(3)) * deltaT;
-obj.currentPose(2) = obj.currentPose(2) + obj.currentVelocity* sind(obj.currentPose(3)) * deltaT;
+resultantVelocity = sqrt(obj.currentVelocity(1)^2 + obj.currentVelocity(2)^2);
+
+
+obj.currentPose(1) = obj.currentPose(1) + resultantVelocity * cosd(obj.currentPose(3)) * deltaT;
+obj.currentPose(2) = obj.currentPose(2) + resultantVelocity * sind(obj.currentPose(3)) * deltaT;
+
+
+
+
+accleration = obj.accleration;
+velocity = obj.currentVelocity
+position = obj.currentPose(1:2);
+headingRate = obj.headingRate;
+Heading = obj.currentPose(3);
+
+plot(obj.currentPose(1), obj.currentPose(2), 'ro');
+hold on;
+plot(obj.destinationPose(1), obj.destinationPose(2), 'bx');
+xlabel('X Position');
+ylabel('Y Position');
+title('Robot Position');
+drawnow;
+
 
 end
