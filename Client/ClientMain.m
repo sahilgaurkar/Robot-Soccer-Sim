72af8d1e35976f699a20e0b9b346cc2a68a554a2
Wikian233 <gggg1684@gmail.com>
2023-03-12 10:41:59 +0000

    updates

--- Client/ClientMain.m
+++ Client/ClientMain.m
@@ -1,23 +1,18 @@
 %% Client Parameters
 shootDistThres = 10; % Threshold distance from the goal to kick ball [m]
 
-%% Initialize Robots
-ball = Ball(1,1);
-team1{1} = Robot('Home','h1','Attacker');
-team1{2} = Robot('Home','h2','Defender');
-team1{3} = Robot('Home','h3','Supporter');
-team1{4} = Robot('Home','h4','Goalkeeper');
+%% Server Parameters
 
-team2{1} = Robot('Away','a1','Attacker');
-team2{2} = Robot('Away','a2','Defender');
-team2{3} = Robot('Away','a3','supporter');
-team2{4} = Robot('Away','a4','Goalkeeper');
+%% Monitor Parameters
+
+%% Initialize Robots
+
 
 %% FOR TESTING CODE
-a = [[1,1]; [2,2]; [3,3]];
-b = [[0,0]; [1,1]; [2,2]];
+c = [[1,1,0]; [2,2,1]; [3,3,2]];
+b = [[0,0,1]; [1,1,2]; [2,2,3]];
 % dist = [];
 for idx=1:1:3
-dist(idx) = distToGoal(b(idx,:), a(idx,:));
+dist(idx) = distToGoal(b(idx,1:2), c(idx,1:2));
 end
 dist
