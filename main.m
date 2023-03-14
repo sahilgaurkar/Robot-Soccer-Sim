1accdb9d0cc3c7e6bb2be2d48872f4e22f7dc58b
Wikian233 <gggg1684@gmail.com>
2023-03-14 10:32:53 +0000

    try to add a parameters initiating function

---
+++ main.m
@@ -0,0 +1,7 @@
+%% prarameters
+shootDistThres = 2.5;
+kickableAreaRadius = 0.1;
+homeGoalPose = [4.5, 0];
+awayGoalPose = [-4.5, 0];
+AttackPositions = [[2.5,0,0]; [1,2,0]; [1,-2,0]];
+DefendPositions = [[-2,0,0]; [-3,2,0]; [-3,-2,0]];
